6bd8f294eded3f4cdc25c24a1866770055054053
Muhammad <muhammadraza0047@gmail.com>
2017-09-09 23:58:59 -0400

    HexClock

---
+++ clock.js
@@ -0,0 +1,30 @@
+var clock = document.getElementById('clock');
+var hexColor = document.getElementById('hex-color');
+
+function hexClock (){
+var time = new Date();
+var hours = (time.getHours() % 12).toString();
+
+var minutes = time.getMinutes().toString();
+var seconds = time.getSeconds().toString();
+
+
+
+
+if (hours.length < 2){
+	hours = "0" + hours;
+}
+if (minutes.length < 2){
+	minutes = "0" + minutes;
+}
+if(seconds.length < 2){
+	seconds ="0" + seconds;
+}
+var clockStr = hours + ':' + minutes + '.' + seconds;
+var hexColorStr = '#' + hours + minutes + seconds;
+clock. textContent = clockStr ;
+hexColor.textContent= hexColorStr;
+document.body.style.backgroundColor = hexColorStr;
+
+}
+setInterval(hexClock, 1000);
